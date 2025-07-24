fd1dc6448969bfc2365108b625cfd1931369af3c
Abbas Chitsazi <abaschitsazii@gmail.com>
2025-07-24 13:44:37 +0330

    breadcrumb is show and index implemented

--- resources/views/components/job-card.blade.php
+++ resources/views/components/job-card.blade.php
@@ -1,4 +1,4 @@
- <x-card class="mb-4">
+<x-card class="mb-4">
         <div class=" mb-4 flex justify-between">
             <h2 class="text-lg font-medium">{{$job->title}}</h2>
             <div class="text-slate-500">{{number_format($job->salary)}}</div>
@@ -17,4 +17,4 @@
             {!! (nl2br(e($job->description))) !!}
         </p>
         {{$slot}}
-    </x-card>
+</x-card>
