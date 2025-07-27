eaf479e22c55580c693253cbbb785709b9e1605b
Abbas Chitsazi <abaschitsazii@gmail.com>
2025-07-27 03:54:00 +0330

    refactor css of apply job

--- resources/views/components/text-input.blade.php
+++ resources/views/components/text-input.blade.php
@@ -1,16 +1,36 @@
 <div class="relative">
-    @if ($formRef && $value)
-        <button type="button" class="absolute top-0 right-0 flex h-full items-center pr-2"
-            @click="$refs['input-{{ $name }}'].value = '';$refs['{{ $formRef }}'].submit(); ">
-            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
-                stroke="currentColor" class="h-5 w-5 text-slate-500">
-                <path stroke-linecap="round" stroke-linejoin="round" d="M6 18 18 6M6 6l12 12" />
-            </svg>
-        </button>
+    @if ('textarea' != $type)
+        @if ($formRef && $value)
+            <button type="button" class="absolute top-0 right-0 flex h-full items-center pr-2"
+                @click="$refs['input-{{ $name }}'].value = '';$refs['{{ $formRef }}'].submit(); ">
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
+                    stroke="currentColor" class="h-5 w-5 text-slate-500">
+                    <path stroke-linecap="round" stroke-linejoin="round" d="M6 18 18 6M6 6l12 12" />
+                </svg>
+            </button>
+        @endif
+        <input x-ref="input-{{ $name }}" type="{{ $type }}" placeholder="{{ $placeholder }}"
+            name="{{ $name }}" value="{{ old($name, $value) }}" id="{{ $name }}"
+            @class([
+                'w-full rounded-md border-0 py-2 px-3 text-sm ring-2 placeholder:text-slate-400 focus:ring-3',
+                'pr-8' => $formRef,
+                'file:mr-4 file:py-1 file:px-3',
+                'file:rounded-md file:border-0',
+                'file:text-sm file:font-semibold',
+                'file:bg-indigo-50 file:text-indigo-700',
+                'hover:file:bg-indigo-100',
+                'ring-slate-300' => !$errors->has($name),
+                'ring-red-300' => $errors->has($name),
+            ]) />
+    @else
+        <textarea @class([
+            'w-full rounded-md border-0 py-1.5 px-3 text-sm ring-2 placeholder:text-slate-400 focus:ring-3',
+            'pr-8' => $formRef,
+            'ring-slate-300' => !$errors->has($name),
+            'ring-red-300' => $errors->has($name),
+        ]) value="{{ old($name, $value) }}" name="{{ $name }}"
+            id="{{ $name }}"></textarea>
     @endif
-    <input x-ref="input-{{ $name }}" type="{{ $type }}" placeholder="{{ $placeholder }}"
-        name="{{ $name }}" value="{{ $value }}" id="{{ $name }}"
-        {{ $attributes->class(['w-full rounded-md border-0 py-1.5 px-3 text-sm ring-2 ring-slate-300 placeholder:text-slate-400 focus:ring-3']) }} />
     @error($name)
         <div class="mt-1 text-sm text-red-500">
             {{ $message }}
--- resources/views/job_application/create.blade.php
+++ resources/views/job_application/create.blade.php
@@ -11,14 +11,11 @@
                 <div class="mb-4">
                     <label for="expected_salary" class="mb-2 block text-base font-normal text-slate-900">Expected
                         Salary</label>
-                    <x-text-input class="py-2.5" type="number" name="expected_salary" />
+                    <x-text-input type="number" name="expected_salary" />
                 </div>
                 <div class="mb-4">
                     <label for="cv" class="mb-2  block text-base font-normal text-slate-900">Upload CV</label>
-                    <x-text-input
-                        class="text-gray-500 file:mr-2 file:py-1.5 file:px-4 file:rounded file:border-0 file:text-sm file:font-semiboldfile:bg-indigo-50 file:text-indigo-700 file:bg-indigo-100"
-                        type="file" name="cv" />
-
+                    <x-text-input class="" type="file" name="cv" />
                 </div>
                 <x-button class="w-full cursor-pointer text-green-600">
                     Apply
