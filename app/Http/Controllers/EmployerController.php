1248a264f0a55178157f6104e483fcb9fb4e81db
Abbas Chitsazi <abaschitsazii@gmail.com>
2025-07-27 14:35:35 +0330

    create employer controller and  implement policies

---
+++ app/Http/Controllers/EmployerController.php
@@ -0,0 +1,31 @@
+<?php
+namespace App\Http\Controllers;
+
+
+use App\Models\Employer;
+use Illuminate\Http\Request;
+use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
+
+class EmployerController extends Controller
+{
+    use AuthorizesRequests;
+
+    public function create()
+    {
+        $this->authorize('create', Employer::class);
+        return view('employer.create');
+    }
+
+    public function store(Request $request)
+    {
+        $this->authorize('create', Employer::class);
+
+        auth()->user()->employer()->create(
+            $request->validate([
+                'company_name' => 'required|min:3|unique:employers,company_name'
+            ])
+        );
+
+        return redirect()->route('jobs.index')->with('success','Your employer account was created!');
+    }
+}
